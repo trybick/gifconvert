389d0bf96830e0d9d07a666540fb02d2adfdc29b
Tim Rybicki <t.rybicki@gmail.com>
2020-12-04 20:12:29 -0500

    Remove css and web vitals

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import React from 'react';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edisst <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Hey there</div>;
 }
 
 export default App;
