4bcb8e27a38711ac0009fce3ea1c493aefe9832f
Tim Rybicki <t.rybicki@gmail.com>
2020-12-05 10:35:41 -0500

    Move createWindow into helper file

--- electron/main.ts
+++ electron/main.ts
@@ -1,46 +1,19 @@
 import { app, BrowserWindow } from 'electron';
-import * as path from 'path';
-import * as isDev from 'electron-is-dev';
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+import { createWindow } from './helpers/startup';
 
 let mainWindow: BrowserWindow | null = null;
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 1000,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: true,
-    },
-  });
-
-  if (isDev) {
-    mainWindow.loadURL('http://localhost:4002/index.html');
-
-    require('electron-reload')(__dirname, {
-      electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
-      forceHardReset: true,
-      hardResetMethod: 'exit',
-    });
-
-    mainWindow.webContents.openDevTools();
-  } else {
-    mainWindow.loadURL(`file://${__dirname}/../index.html`);
-  }
-
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
-
-  mainWindow.on('closed', () => (mainWindow = null));
+function init() {
+  createWindow(mainWindow, { devToolsOnStartup: false });
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+  init();
+});
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    init();
   }
 });
 
